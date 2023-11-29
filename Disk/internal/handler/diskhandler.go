33de93b2bbbad3db263afc44bcacdac8133603a9
Khalil996 <2649475267@qq.com>
2023-11-30 00:15:09 +0800

    init

---
+++ Disk/internal/handler/diskhandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"cloud_go/Disk/internal/logic"
+	"cloud_go/Disk/internal/svc"
+	"cloud_go/Disk/internal/types"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func DiskHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.Request
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := logic.NewDiskLogic(r.Context(), svcCtx)
+		resp, err := l.Disk(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.OkJsonCtx(r.Context(), w, resp)
+		}
+	}
+}
